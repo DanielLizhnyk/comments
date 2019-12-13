b93aa68cdcc2243fff90d9c8f80b212e26635b71
Daniel Lizhnyk <47256401+DanielLizhnyk@users.noreply.github.com>
2019-12-13 12:37:49 +0200

    Add files via upload

---
+++ storage/framework/views/c36749183afc3593209021966db5a3e9db52df87.php
@@ -0,0 +1,29 @@
+<?php $__env->startSection('content'); ?>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <table class="table table-striped">
+                    <thead>
+                    <th>ID</th>
+                    <th>Title</th>
+                    <th>Action</th>
+                    </thead>
+                    <tbody>
+                    <?php $__currentLoopData = $posts; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $post): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                        <tr>
+                            <td><?php echo e($post->id); ?></td>
+                            <td><?php echo e($post->title); ?></td>
+                            <td>
+                                <a href="<?php echo e(route('post.show', $post->id)); ?>" class="btn btn-primary">Show Post</a>
+                            </td>
+                        </tr>
+                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                    </tbody>
+
+                </table>
+            </div>
+        </div>
+    </div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\OSPanel\domains\comments\resources\views/index.blade.php ENDPATH**/ ?>
